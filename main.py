f1cba1d0b1790b61489605f2e814c516db55203a
shenoyshre22 <shreyashenoy0222@gmail.com>
2025-11-02 20:15:34 +0530

    imports error flag from the changed parser file now and marks error if any

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # main.py
-# This file is based on the structure in 7.3 main.py of your tutorial [cite: 360]
 
-from lexer import lexer   # [cite: 361]
-from parser import parser # [cite: 362]
+from lexer import lexer
+from parser import parser, PARSE_ERROR_FLAG  # Import the flag
 import sys
 
 print("CSS Syntax Validator")
@@ -10,32 +9,25 @@ print("Enter your CSS code.")
 print("On a new line, press Ctrl+D (Linux/Mac) or Ctrl+Z+Enter (Windows) when done.")
 print("-----------------------------------------------------------------------------")
 
-# Read all lines of input from the user
 try:
     input_data = ""
     while True:
         line = input()
         input_data += line + '\n'
 except EOFError:
-    pass # User has finished entering input
+    pass
 
 if not input_data.strip():
     print("No input provided. Exiting.")
 else:
     print("\n--- Parsing Input ---")
     
-    # Run the parser on the entire input string
-    # The 'lexer' is automatically used by the parser
-    result = parser.parse(input_data) # [cite: 366]
-    
-    # The p_error function (in parser.py) will print any syntax errors.
-    # If no errors were found, 'result' will be None (or the value from the top rule)
-    # and we can consider it "Accepted".
+    result = parser.parse(input_data, lexer=lexer)
     
     print("-----------------------")
-    # This check is based on your tutorial's main file [cite: 367]
-    if result is not None:
-        print(" Syntax: Accepted YAY")
+    
+    # Check the flag INSTEAD of the 'result'
+    if not PARSE_ERROR_FLAG:
+        print("✅ Syntax: Accepted")
     else:
-        # Note: p_error will have already printed the specific error.
-        print(" Syntax: Rejected (See error messages above)")
+        print("❌ Syntax: Rejected (See error messages above)")
